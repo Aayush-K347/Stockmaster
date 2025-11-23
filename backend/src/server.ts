148440da866375a91b024fb499532ca7008aafa1
Aayush-K347 <80696832+Aayush-K347@users.noreply.github.com>
2025-11-23 05:50:07 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,10 +18,15 @@ const app = express();
 
 app.use(
   cors({
-    origin: '*',
+    origin: [
+      "https://stockmaster-red.vercel.app/",
+      "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
 );
+
 app.use(helmet());
 app.use(express.json({ limit: '1mb' }));
 app.use(
